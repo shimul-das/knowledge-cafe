4ff4732f1e14c28d44317cfbcb411a94de9e21ef
Shimul Das <shimul.191002110@gmail.com>
2023-03-30 23:36:28 +0600

    Header Done

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -5,11 +5,11 @@ import logo from '../images/Logo.svg'
 const Header = () => {
   return (
     <nav className='header'>
-        <img src={logo} alt="" />
+        <h2 className='logo_title'>Knowledge Cafe</h2>
         <div className='h_link'>
-        <a href='/shop'>Shop</a>
-        <a href='/order'>Order</a>
-        <a href='/inventory'>Inventory</a>
+        <a href='/shop'>Blogs</a>
+        <a href='/order'>Contact</a>
+        <a href='/inventory'>Team</a>
         <a href='/login'>Login</a>
         </div>
         
